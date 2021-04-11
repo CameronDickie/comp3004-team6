cf57ec38056b91da8e30039553050eebea10ea8a
benwilliams140 <ben.williams14@outlook.com>
2021-04-11 19:35:46 -0400

    Updated strategy for submitting a deliverable, now only accepting one document - Ben Williams

--- src/main/java/com/comp3004/educationmanager/strategy/SubmitDeliverableStrategy.java
+++ src/main/java/com/comp3004/educationmanager/strategy/SubmitDeliverableStrategy.java
@@ -1,15 +1,17 @@
 package com.comp3004.educationmanager.strategy;
 
 import com.comp3004.educationmanager.composite.Component;
-import com.comp3004.educationmanager.composite.CourseContent;
+import com.comp3004.educationmanager.composite.CourseItem;
 import com.comp3004.educationmanager.decorator.EditableDecorator;
+import com.comp3004.educationmanager.decorator.FileDecorator;
 import com.comp3004.educationmanager.decorator.GradeableDecorator;
 
 public class SubmitDeliverableStrategy implements Strategy {
     @Override
     public Component createCourseItem(String name, String path, String type, long userID, String userType, boolean visible) {
-        Component content = new CourseContent(name, path, type, userID, userType, visible);
+        Component content = new CourseItem(name, path, type, userID, userType, visible);
         content = new GradeableDecorator(content);
+        content = new FileDecorator(content);
         content = new EditableDecorator(content);
         return content;
     }
